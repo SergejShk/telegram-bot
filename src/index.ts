a63aa81f3d923b5ffc6c7f7f889765c3f884b1be
SergejShk <sshkurupij11@gmail.com>
2023-12-08 13:30:14 +0200

    Created server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
-import TelegramBot from "node-telegram-bot-api";
+import express from "express";
+import cors from "cors";
+// import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const botToken = process.env.TL_BOT_TOKEN || "";
+const PORT = process.env.PORT || 3000;
+// const botToken = process.env.TL_BOT_TOKEN || "";
+const app = express();
 
-const bot = new TelegramBot(botToken, { polling: true });
+app.use(cors());
+app.use(express.json());
 
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, "Received your message");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
+
+// const bot = new TelegramBot(botToken, { polling: true });
+
+// bot.on("message", (msg) => {
+//   const chatId = msg.chat.id;
+
+//   bot.sendMessage(chatId, "Received your message");
+// });
