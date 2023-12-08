30cce1a9d981b4ad26e6a20eeb6fdbd97406d4d8
SergejShk <sshkurupij11@gmail.com>
2023-12-08 14:22:55 +0200

    Connected bot

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,37 @@
 import express from "express";
 import cors from "cors";
-// import TelegramBot from "node-telegram-bot-api";
+import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-// const botToken = process.env.TL_BOT_TOKEN || "";
+const botToken = process.env.TL_BOT_TOKEN || "";
+const apiUrl = process.env.API_URL || "";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server is working!");
+});
+
+app.post("/", (_, res) => {
+  res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
-// const bot = new TelegramBot(botToken, { polling: true });
+const bot = new TelegramBot(botToken, { polling: true });
 
-// bot.on("message", (msg) => {
-//   const chatId = msg.chat.id;
+bot.setWebHook(apiUrl);
 
-//   bot.sendMessage(chatId, "Received your message");
-// });
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "Received your message");
+});
