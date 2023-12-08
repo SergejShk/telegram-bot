6aec6be23e71be5db2aee801a7c3cf6a6dd0728f
SergejShk <sshkurupij11@gmail.com>
2023-12-08 15:25:48 +0200

    Removed express

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,19 @@
-import express from "express";
-import cors from "cors";
 import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 const botToken = process.env.TL_BOT_TOKEN || "";
-// const apiUrl = process.env.API_URL || "";
-const tlWebhookUrl = process.env.TL_WEBHOOK_URL || "";
+const apiUrl = process.env.API_URL || "";
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (_, res) => {
-  res.send("Server is working!");
+const bot = new TelegramBot(botToken, {
+  webHook: {
+    port: PORT,
+  },
 });
 
-app.get("bot", (req, res) => {
-  if (!req.body) return res.sendStatus(200);
-  const { body } = req.body;
-
-  bot.processUpdate(body);
-  res.sendStatus(200);
-});
-
-app.post("bot", (req, res) => {
-  if (!req.body) return res.sendStatus(200);
-  const { body } = req.body;
-
-  bot.processUpdate(body);
-  res.sendStatus(200);
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
-
-const bot = new TelegramBot(botToken, { polling: true });
-
-bot.setWebHook(`${tlWebhookUrl}/bot`);
+bot.setWebHook(`${apiUrl}/bot${botToken}`);
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
